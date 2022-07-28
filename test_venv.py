b1bc458ac50d48b989d93d657e5bd92715d3b2ed
pympk <yee_ping@yahoo.com>
2022-07-27 20:42:45 -0700

    add myUtils

--- test_venv.py
+++ test_venv.py
@@ -12,4 +12,36 @@ print("pandas_version: ", pd.__version__)
 print("numpy_version: ", np.__version__)
 if __name__ == "__main__":
     # If the modules can't be imported, the following print won't happen
-    print("Successfully imported the modules!")
+    print("Successfully imported the modules!", "\n")
+
+# # works in vscode and colab
+# import sys
+# # sys.path.insert(0, 'C:/Users/ping/MyDrive/py_files/python/utils')
+# sys.path.insert(0, '/content/drive/MyDrive/py_files/python/utils')
+# from utils import sum_numbers
+# print(sum_numbers.sum_2(1, 2))
+
+# works in vscode and colab, give path to utils.py
+import sys
+sys.path.insert(0, 'C:/Users/ping/MyDrive/py_files/python/utils/utils')
+# sys.path.append('/content/drive/MyDrive/py_files/python/utils/utils')
+import sum_numbers as sn
+print(sn.sum_2(1, 2))
+
+
+
+# import utils
+# print(sum_numbers.sum_3(1, 2, 3), "\n")
+
+
+# # works if pip install utils in vscode
+# from utils import sum_numbers as mySum
+# print(mySum.sum_2(1, 2))
+# print(mySum.sum_3(1, 2, 3), "\n")
+
+# import utils as utl
+# print(f'{utl.sum_numbers.sum_2(10, 10)}')
+
+
+
+
