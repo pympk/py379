b2462a312a3e6f3d77bab7ee3f2fc55474ec57f5
pympk <yee_ping@yahoo.com>
2022-08-14 19:52:05 -0700

    in progress

---
+++ trash.py
@@ -0,0 +1,5 @@
+import plotly
+from myUtils import symb_perf_stats, OBV_calc, UI_MW
+import plotly.graph_objs as go
+
+print(plotly.__version__)
---
+++ yf_plot_mySymbols.py
@@ -0,0 +1,27 @@
+from myUtils import yf_plot_symbols  # NOQA
+
+path_dir = "C:/Users/ping/MyDrive/stocks/MktCap2b_AUMtop1200/"
+path_data_dump = path_dir + "VSCode_dump/"
+filename_pickled_df_OHLCV = (
+    "df_OHLCV"  # pickled filename reindexed to NYSE dates
+)
+
+# symbols = ['FTEC', 'BCI', 'BTC-USD', 'ETH-USD']
+symbols = ["FTEC", "BCI"]
+
+# date_end_limit = None
+date_end_limit = "2022-08-12"
+# iloc_offset = None  # number of days to plot
+iloc_offset = 252  # number of days to plot
+date_start_limit = None
+
+caches_returned_by_plot = []
+cache_returned_by_plot = yf_plot_symbols(
+    symbols,
+    path_data_dump,
+    filename_pickled_df_OHLCV,
+    date_start_limit,
+    date_end_limit,
+    iloc_offset,
+)
+caches_returned_by_plot.append(cache_returned_by_plot)
