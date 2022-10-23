af5949c7dd2200d26ee68f7b6e4494a463cde031
pympk <yee_ping@yahoo.com>
2022-10-23 10:30:17 -0700

    a

--- trash.py
+++ trash.py
@@ -1,6 +1,15 @@
-df_merged = df.merge(dfw, how='left', on='Date')
-df_merged = df_merged.sort_values(by=['Date', 'Time'])
-df_merged.reset_index(drop=True, inplace=True)  # drop current index
-pickle_dump(df_merged, path_pickle_dump, 'df_grSale_weather')
-df_merged = pickle_load(path_pickle_dump, 'df_grSale_weather')
-df_merged
+# print df of each unique values in level 0
+# 0 = 0
+_idx_names = _df.index.names
+_dict = {'DOW': 'Days-of-week', 0:'Sun', 1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat'}
+_dict[_idx_names[0]]
+items_in0 = _df.index.unique(level=0)
+# print(f'items_in0_{0}: {items_in0}')
+# print(f'items in level: {0} (index name: {_idx_names[0]}): {items_in0}')  
+print(f'items in level: {0}, index name: {_idx_names[0]} ({_dict[_idx_names[0]]}): {items_in0}\n')  
+for unique_item in items_in0:
+  print(f'level {0}, unique item: {unique_item} ({_dict[unique_item]})')
+  # print(_df.loc[unique_item].index.names[0])    
+  print(_df.loc[unique_item].index.names[0])  
+  print(_df.loc[unique_item].index.unique(level=0))
+  print('')
