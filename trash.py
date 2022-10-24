f5c7aed794fd66ad9238eed31678375a3ac224e1
pympk <yee_ping@yahoo.com>
2022-10-24 16:15:25 -0700

    a

--- trash.py
+++ trash.py
@@ -1,15 +1,11 @@
-# print df of each unique values in level 0
-# 0 = 0
-_idx_names = _df.index.names
-_dict = {'DOW': 'Days-of-week', 0:'Sun', 1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat'}
-_dict[_idx_names[0]]
-items_in0 = _df.index.unique(level=0)
-# print(f'items_in0_{0}: {items_in0}')
-# print(f'items in level: {0} (index name: {_idx_names[0]}): {items_in0}')  
-print(f'items in level: {0}, index name: {_idx_names[0]} ({_dict[_idx_names[0]]}): {items_in0}\n')  
-for unique_item in items_in0:
-  print(f'level {0}, unique item: {unique_item} ({_dict[unique_item]})')
-  # print(_df.loc[unique_item].index.names[0])    
-  print(_df.loc[unique_item].index.names[0])  
-  print(_df.loc[unique_item].index.unique(level=0))
-  print('')
+df_1_5 =df_dow_g.loc[1:5]
+# df_1_5
+df_1_5.columns = df_1_5.columns.droplevel(1)  # drop sum in column index
+df_1_5.index = df_1_5.index.droplevel(0)  # drop DOW in row index
+df_1_5 = df_1_5.groupby(['Item']).agg({'Date':['sum'], 'Qty':['sum'], 'Gross Sales':['sum']})
+df_1_5.columns = df_1_5.columns.droplevel(1)  # drop sum in column index
+df_1_5['Avg_Sales_per_day'] = df_1_5['Gross Sales'] / df_1_5['Date'] 
+df_1_5['Avg_Qty_per_day'] = df_1_5['Qty'] / df_1_5['Date'] 
+df_1_5 = df_1_5.sort_values(by=['Gross Sales'], ascending=False)
+df_1_5 = df_1_5.head(15)
+df_1_5
