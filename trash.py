45cbc959834386dd32787e2c6dbc5301b07418bb
pympk <yee_ping@yahoo.com>
2022-12-01 19:49:24 -0800

    a

--- trash.py
+++ trash.py
@@ -1,39 +1,39 @@
-perf_ranks_dict = {}  # dic of performance ranks
-syms_perf_rank = []  # list of lists to store top 100 ranked symbols
-days_lookbacks = [-15, -30, -60, -120, -240]
-for days_lookback in days_lookbacks:
-  f_name = 'period' + str(days_lookback)
+def random_slices (df, n_samples, days_lookback, days_eval):
+  """Pad target_arr with leading numpy.nan to length arr_len.
 
-  _df_c = df_c[days_lookback::]
-  symbols, period_yr, drawdown, UI, max_drawdown, returns_std, Std_UI, CAGR, CAGR_Std, CAGR_UI = \
-      symb_perf_stats_vectorized(_df_c)
-  caches_perf_stats_vect = []
-  for symbol in symbols:
-      date_first = drawdown.index[0].strftime('%Y-%m-%d')
-      date_last = drawdown.index[-1].strftime('%Y-%m-%d')
-      cache = (symbol, date_first, date_last, period_yr, CAGR[symbol],
-              UI[symbol], Std_UI[symbol], CAGR_Std[symbol], CAGR_UI[symbol])
-      # append performance data (tuple) to caches_perf_stats (list)
-      caches_perf_stats_vect.append(cache)
-  column_names = ['symbol', 'first date', 'last date', 'Year', 'CAGR',
-                  'UI', 'Std/UI', 'CAGR/Std', 'CAGR/UI']
+  Args:
+      df(dataframe): dataframe
+      n_samples(int): number of slices to return
+      days_lookback(int):  number of days to lookback for training
+      days_eval(int): number of days to forward for evaluation 
 
-  # write symbols' performance stats to dataframe
-  df_ps = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
-  df_ps['r_CAGR/UI'] = df_ps['CAGR/UI'].rank(ascending=False)
-  df_ps['r_CAGR/Std'] = df_ps['CAGR/Std'].rank(ascending=False)
-  df_ps['r_Std/UI'] = df_ps['Std/UI'].rank(ascending=False)
+  Return:
+      r_slices(list of tuples): target_arr padded to length arr_len
+  """
   
-  _dict = {}
-  cols_sort = ['r_CAGR/UI', 'r_CAGR/Std', 'r_Std/UI']
-  # print(f'{f_name} top 100 symbols')  
-  for col in cols_sort:
-    symbols_top_100 = df_ps.sort_values(by=[col]).head(100).symbol.values
-    syms_perf_rank.append(list(symbols_top_100))
-    # print(f'{col}: {symbols_top_100}')
-    _dict[col] = symbols_top_100
-    perf_ranks_dict[f'{f_name}'] = _dict
-  # print(' ')
+  
+  import random
+  from random import randint
 
-pickle_dump(perf_ranks_dict, path_data_dump, f_pickled_perf_ranks_dict)
-print(f'perf_ranks_dict:\n{perf_ranks_dict}\n')
+  # random.seed(0)  
+  n_sample = 0
+  days_total = days_lookback + days_eval
+  print(f'days_lookback: {days_lookback}, days_eval: {days_eval}, days_total: {days_total}, len(df): {len(df)}')
+
+  if days_total > len(df):
+    msg_err = f'days_total: {days_total} must be less or equal to len(df): {len(df)}'
+    raise SystemExit(msg_err)
+
+  # random slices of iloc for train and eval that fits the days_lookback, days_eval and total len(df) constraints
+  r_slices = []
+  while n_sample < n_samples:
+    n_rand = randint(0, len(df))    
+    start_train = n_rand - days_lookback
+    end_train = n_rand
+    start_eval = n_rand
+    end_eval = n_rand + days_eval
+    if 0 <= start_train and end_eval <= len(df):
+      r_slices.append((start_train, end_train, end_eval))
+      n_sample += 1
+
+  return r_slices    
