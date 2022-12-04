1d3867900ae438aa7f7751600c2acc8fcc4d7af4
pympk <yee_ping@yahoo.com>
2022-12-04 11:29:43 -0800

    a

--- trash.py
+++ trash.py
@@ -1,39 +1,47 @@
-def random_slices (df, n_samples, days_lookback, days_eval):
-  """Pad target_arr with leading numpy.nan to length arr_len.
+def random_slices(len_df, n_samples, days_lookback, days_eval, verbose=False):
+    """Returns a list of random tuples of start_train, end_train, end_eval, where
+        iloc[start_train:end_train] is used for training, and iloc[end_train:end_eval]
+        is used for evaluation.  The length of the list is equal to n_samples.
+        i.e. [(248, 368, 388), (199, 319, 339), ... (45, 165, 185)]
 
-  Args:
-      df(dataframe): dataframe
-      n_samples(int): number of slices to return
-      days_lookback(int):  number of days to lookback for training
-      days_eval(int): number of days to forward for evaluation 
+    Args:
+        len_df(int): length of dataframe
+        n_samples(int): number of slices to return
+        days_lookback(int):  number of days to lookback for training
+        days_eval(int): number of days forward for evaluation
 
-  Return:
-      r_slices(list of tuples): target_arr padded to length arr_len
-  """
-  
-  
-  import random
-  from random import randint
+    Return:
+        r_slices(list of tuples): list of random tuples of start_train, end_train, end_eval, where
+          iloc[start_train:end_train] is used for training, and iloc[end_train:end_eval] is used for
+          evaluation.
+          i.e. [(248, 368, 388), (199, 319, 339), ... (45, 165, 185)]
+    """
 
-  # random.seed(0)  
-  n_sample = 0
-  days_total = days_lookback + days_eval
-  print(f'days_lookback: {days_lookback}, days_eval: {days_eval}, days_total: {days_total}, len(df): {len(df)}')
+    import random
+    from random import randint
 
-  if days_total > len(df):
-    msg_err = f'days_total: {days_total} must be less or equal to len(df): {len(df)}'
-    raise SystemExit(msg_err)
+    # random.seed(0)
+    n_sample = 0
+    days_total = days_lookback + days_eval
+    if verbose:
+        print(
+            f"days_lookback: {days_lookback}, days_eval: {days_eval}, days_total: {days_total}, len_df: {len_df}"
+        )
 
-  # random slices of iloc for train and eval that fits the days_lookback, days_eval and total len(df) constraints
-  r_slices = []
-  while n_sample < n_samples:
-    n_rand = randint(0, len(df))    
-    start_train = n_rand - days_lookback
-    end_train = n_rand
-    start_eval = n_rand
-    end_eval = n_rand + days_eval
-    if 0 <= start_train and end_eval <= len(df):
-      r_slices.append((start_train, end_train, end_eval))
-      n_sample += 1
+    if days_total > len_df:
+        msg_err = f"days_total: {days_total} must be less or equal to len_df: {len_df}"
+        raise SystemExit(msg_err)
 
-  return r_slices    
+    # random slices of iloc for train and eval that fits the days_lookback, days_eval and total len_df constraints
+    r_slices = []
+    while n_sample < n_samples:
+        n_rand = randint(0, len_df)
+        start_train = n_rand - days_lookback
+        end_train = n_rand
+        # start_eval = n_rand
+        end_eval = n_rand + days_eval
+        if 0 <= start_train and end_eval <= len_df:
+            r_slices.append((start_train, end_train, end_eval))
+            n_sample += 1
+
+    return r_slices
--- yf_utils.py
+++ yf_utils.py
@@ -1,9 +1,21 @@
 def split_train_val_test(
     df, s_train=0.7, s_val=0.2, s_test=0.1, verbose=False
 ):
-    """'
-    Split df into df_train, df_val and df_test and returns the splitted dfs
+    """Split df into training (df_train), validation (df_val)
+    and test (df_test) and returns the splitted dfs
+
+    Args:
+        df(dataframe): dataframe to be splitted
+        s_train(float): ratio of df_train / df
+        s_val(float): ratio of df_val / df
+        s_test(float): ratio of df_test / df
+
+    Return:
+        df_train(dataframe): training portion of df
+        df_val(dataframe): validation portion of df
+        df_test(dataframe): test portion of df
     """
+    
     if (s_train + s_val + s_test) - 1 > 0.0001:  # allow 0.01% error
         _sum = s_train + s_val + s_test
         raise Exception(
@@ -30,3 +42,52 @@ def split_train_val_test(
         print(f"sum_train_val_test: {sum_train_val_test}")
 
     return df_train, df_val, df_test
+
+
+def random_slices(len_df, n_samples, days_lookback, days_eval, verbose=False):
+    """Returns a list of random tuples of start_train, end_train, end_eval, where
+        iloc[start_train:end_train] is used for training, and iloc[end_train:end_eval]
+        is used for evaluation.  The length of the list is equal to n_samples.
+        i.e. [(248, 368, 388), (199, 319, 339), ... (45, 165, 185)]
+
+    Args:
+        len_df(int): length of dataframe
+        n_samples(int): number of slices to return
+        days_lookback(int):  number of days to lookback for training
+        days_eval(int): number of days forward for evaluation
+
+    Return:
+        r_slices(list of tuples): list of random tuples of start_train, end_train, end_eval, where
+          iloc[start_train:end_train] is used for training, and iloc[end_train:end_eval] is used for
+          evaluation.
+          i.e. [(248, 368, 388), (199, 319, 339), ... (45, 165, 185)]
+    """
+
+    import random
+    from random import randint
+
+    # random.seed(0)
+    n_sample = 0
+    days_total = days_lookback + days_eval
+    if verbose:
+        print(
+            f"days_lookback: {days_lookback}, days_eval: {days_eval}, days_total: {days_total}, len_df: {len_df}"
+        )
+
+    if days_total > len_df:
+        msg_err = f"days_total: {days_total} must be less or equal to len_df: {len_df}"
+        raise SystemExit(msg_err)
+
+    # random slices of iloc for train and eval that fits the days_lookback, days_eval and total len_df constraints
+    r_slices = []
+    while n_sample < n_samples:
+        n_rand = randint(0, len_df)
+        start_train = n_rand - days_lookback
+        end_train = n_rand
+        # start_eval = n_rand
+        end_eval = n_rand + days_eval
+        if 0 <= start_train and end_eval <= len_df:
+            r_slices.append((start_train, end_train, end_eval))
+            n_sample += 1
+
+    return r_slices
