bb3aecf194fc134584981c581e76f5488260cbbf
pympk <yee_ping@yahoo.com>
2022-12-20 21:24:53 -0800

    a

--- myUtils.py
+++ myUtils.py
@@ -2465,7 +2465,7 @@ def symb_perf_stats_vectorized_v1(df_symbols_close):
 
 def symb_perf_stats_vectorized_v2(df_symbols_close):
     """Takes dataframe of symbols' close and returns symbols, period_yr,
-       drawdown, UI, max_drawdown, returns_std, Std_UI, CAGR, CAGR_Std, CAGR_UI
+       drawdown, UI, max_drawdown, returns_std, returns_std_div_UI, CAGR, CAGR_div_Std, CAGR_div_UI
        https://stackoverflow.com/questions/36750571/calculate-max-draw-down-with-a-vectorized-solution-in-python
        http://www.tangotools.com/ui/ui.htm
        Calculation CHECKED against: http://www.tangotools.com/ui/UlcerIndex.xls
@@ -2483,10 +2483,10 @@ def symb_perf_stats_vectorized_v2(df_symbols_close):
         UI(pandas.series float64): ulcer-index
         max_drawdown(pandas series float64): maximum drawdown from peak
         returns_std(pandas series float64): standard deviation of daily returns
-        Std_UI(pandas series float64): returns_std / UI
+        returns_std_div_UI(pandas series float64): returns_std / UI
         CAGR(pandas series float64): compounded annual growth rate
-        CAGR_Std(pandas series float64): CAGR / returns_std
-        CAGR_UI(pandas series float64): CAGR / UI
+        CAGR_div_Std(pandas series float64): CAGR / returns_std
+        CAGR_div_UI(pandas series float64): CAGR / UI
     """
     # v1 convert drawdown from pandas series to numpy array
     # v2 do calculation in numpy array
@@ -2500,32 +2500,48 @@ def symb_perf_stats_vectorized_v2(df_symbols_close):
 
     arr = df_symbols_close.to_numpy()
     arr_returns = arr / np.roll(arr, 1, axis=0) - 1
-    arr_returns_std = np.std(arr_returns[1:, :], axis=0, ddof=1)  # drop first row 
-    arr_returns[0] = 0  # set first row to 0 
-    arr_cum_returns = (1 + arr_returns).cumprod(axis=0)  # cumulative product of column elements
-    # accumulative max value of column elements 
-    arr_drawdown = arr_cum_returns / np.maximum.accumulate(arr_cum_returns, axis=0) -1
-    arr_max_drawdown = arr_drawdown.min(axis=0)    
+    arr_returns_std = np.std(
+        arr_returns[1:, :], axis=0, ddof=1
+    )  # drop first row
+    arr_returns[0] = 0  # set first row to 0
+    arr_cum_returns = (1 + arr_returns).cumprod(
+        axis=0
+    )  # cumulative product of column elements
+    # accumulative max value of column elements
+    arr_drawdown = (
+        arr_cum_returns / np.maximum.accumulate(arr_cum_returns, axis=0) - 1
+    )
+    arr_max_drawdown = arr_drawdown.min(axis=0)
     arr_UI = np.sqrt(np.sum(np.square(arr_drawdown), axis=0) / len_df)
-    arr_Std_UI = arr_returns_std / arr_UI
-    Std_UI = pd.Series(arr_Std_UI, index=symbols)
+    arr_returns_std_div_arr_UI = arr_returns_std / arr_UI
+    returns_std_div_UI = pd.Series(arr_returns_std_div_arr_UI, index=symbols)
     period_yr = len_df / 252  # 252 trading days per year
     arr_CAGR = (arr[-1] / arr[0]) ** (1 / period_yr) - 1
-    arr_CAGR_Std = arr_CAGR / arr_returns_std
-    arr_CAGR_UI = arr_CAGR / arr_UI
+    arr_CAGR_div_arr_Std = arr_CAGR / arr_returns_std
+    arr_CAGR_div_arr_UI = arr_CAGR / arr_UI
 
     # convert numpy array to dataframe, add date index and symbols as column names
     drawdown = pd.DataFrame(arr_drawdown, index=dates, columns=symbols)
-    # convert numpy array to pandas series, add symbols as index    
+    # convert numpy array to pandas series, add symbols as index
     UI = pd.Series(arr_UI, index=symbols)
     max_drawdown = pd.Series(arr_max_drawdown, index=symbols)
     returns_std = pd.Series(arr_returns_std, index=symbols)
     CAGR = pd.Series(arr_CAGR, index=symbols)
-    CAGR_Std = pd.Series(arr_CAGR_Std, index=symbols)
-    CAGR_UI = pd.Series(arr_CAGR_UI, index=symbols)
+    CAGR_div_Std = pd.Series(arr_CAGR_div_arr_Std, index=symbols)
+    CAGR_div_UI = pd.Series(arr_CAGR_div_arr_UI, index=symbols)
 
-    return symbols, period_yr, drawdown, UI, max_drawdown, \
-        returns_std, Std_UI, CAGR, CAGR_Std, CAGR_UI
+    return (
+        symbols,
+        period_yr,
+        drawdown,
+        UI,
+        max_drawdown,
+        returns_std,
+        returns_std_div_UI,
+        CAGR,
+        CAGR_div_Std,
+        CAGR_div_UI,
+    )
 
 
 def concat_df(df, columns_drop_duplicate, file_df, file_df_archive, verbose=False):
--- trash.py
+++ trash.py
@@ -1,63 +1,44 @@
-def symb_perf_stats_vectorized_v2(df_symbols_close):
-    """Takes dataframe of symbols' close and returns symbols, period_yr,
-       drawdown, UI, max_drawdown, returns_std, Std_UI, CAGR, CAGR_Std, CAGR_UI
-       https://stackoverflow.com/questions/36750571/calculate-max-draw-down-with-a-vectorized-solution-in-python
-       http://www.tangotools.com/ui/ui.htm
-       Calculation CHECKED against: http://www.tangotools.com/ui/UlcerIndex.xls
-       Calculation VERIFIED in: symb_perf_stats_vectorized.ipynb
+def perf_eval(df_close):
+  '''
+  df_close is a dataframe with date index, columns of symbols' closing price, and symbol as column name.
+  Calculate symbols' performance in drawdown, Ulcer-Index, max-drawdown, standard deviation of returns,
+  standard deviation of returns / Ulcer-Index, CAGR, CAGR / standard deviation of returns, CAGR / Ulcer-Index.
 
-    Args:
-        df_symbols_close(dataframe): dataframe with date as index,
-          symbol's close in columns, and symbols as column names.
+  '''
 
-    Return:
-        symbols(pandas.core.indexes.base.Index): stock symbols
-        period_yr(float): years, (days in dataframe) / 252
-        drawdown(pandas dataframe): drawdown from peak, 0.05 means 5% drawdown,
-            with date index and symbols as column names
-        UI(pandas.series float64): ulcer-index
-        max_drawdown(pandas series float64): maximum drawdown from peak
-        returns_std(pandas series float64): standard deviation of daily returns
-        Std_UI(pandas series float64): returns_std / UI
-        CAGR(pandas series float64): compounded annual growth rate
-        CAGR_Std(pandas series float64): CAGR / returns_std
-        CAGR_UI(pandas series float64): CAGR / UI
-    """
-    # v1 convert drawdown from pandas series to numpy array
-    # v2 do calculation in numpy array
+#   from myUtils import symb_perf_stats_vectorized_v2
+  from trash import symb_perf_stats_vectorized_v2  
 
-    import numpy as np
-    import pandas as pd
+  symbols, period_yr, drawdown, UI, max_drawdown, returns_std, returns_std_div_UI, CAGR, CAGR_div_returns_std, CAGR_div_UI = \
+      symb_perf_stats_vectorized_v2(df_close)
 
-    dates = df_symbols_close.index
-    symbols = df_symbols_close.columns
-    len_df = len(df_symbols_close)
+  caches_perf_stats_vect = []
+  for symbol in symbols:
+      # date_first = drawdown.index[0].strftime('%Y-%m-%d')
+      # date_last = drawdown.index[-1].strftime('%Y-%m-%d')
+      date_first = df_close.index[0].strftime('%Y-%m-%d')
+      date_last = df_close.index[-1].strftime('%Y-%m-%d')
 
-    arr = df_symbols_close.to_numpy()
-    arr_returns = arr / np.roll(arr, 1, axis=0) - 1
-    arr_returns_std = np.std(arr_returns[1:, :], axis=0, ddof=1)  # drop first row 
-    arr_returns[0] = 0  # set first row to 0 
-    arr_cum_returns = (1 + arr_returns).cumprod(axis=0)  # cumulative product of column elements
-    # accumulative max value of column elements 
-    arr_drawdown = arr_cum_returns / np.maximum.accumulate(arr_cum_returns, axis=0) -1
-    arr_max_drawdown = arr_drawdown.min(axis=0)    
-    arr_UI = np.sqrt(np.sum(np.square(arr_drawdown), axis=0) / len_df)
-    arr_Std_UI = arr_returns_std / arr_UI
-    Std_UI = pd.Series(arr_Std_UI, index=symbols)
-    period_yr = len_df / 252  # 252 trading days per year
-    arr_CAGR = (arr[-1] / arr[0]) ** (1 / period_yr) - 1
-    arr_CAGR_Std = arr_CAGR / arr_returns_std
-    arr_CAGR_UI = arr_CAGR / arr_UI
+      cache = (symbol, date_first, date_last, period_yr, CAGR[symbol],
+              UI[symbol], returns_std_div_UI[symbol], CAGR_div_returns_std[symbol], CAGR_div_UI[symbol])
+      # append performance data (tuple) to caches_perf_stats (list)
+      caches_perf_stats_vect.append(cache)
+  column_names = ['symbol', 'first date', 'last date', 'Year', 'CAGR',
+                  'UI', 'return_std/UI', 'CAGR/return_std', 'CAGR/UI']
+  # write symbols' performance stats to dataframe
+  df_perf = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
 
-    # convert numpy array to dataframe, add date index and symbols as column names
-    drawdown = pd.DataFrame(arr_drawdown, index=dates, columns=symbols)
-    # convert numpy array to pandas series, add symbols as index    
-    UI = pd.Series(arr_UI, index=symbols)
-    max_drawdown = pd.Series(arr_max_drawdown, index=symbols)
-    returns_std = pd.Series(arr_returns_std, index=symbols)
-    CAGR = pd.Series(arr_CAGR, index=symbols)
-    CAGR_Std = pd.Series(arr_CAGR_Std, index=symbols)
-    CAGR_UI = pd.Series(arr_CAGR_UI, index=symbols)
+  _cols = ['CAGR', 'UI', 'retrun_std/UI', 'CAGR/return_std', 'CAGR/UI']
+  grp_CAGR_d_UI_mean = df_perf['CAGR/UI'].mean()
+  grp_CAGR_d_UI_std  = df_perf['CAGR/UI'].std()
+  grp_CAGR_d_UI_mean_div_std = grp_CAGR_d_UI_mean / grp_CAGR_d_UI_std 
 
-    return symbols, period_yr, drawdown, UI, max_drawdown, \
-        returns_std, Std_UI, CAGR, CAGR_Std, CAGR_UI
+  grp_CAGR_d_retnStd_mean = df_perf['CAGR/return_std'].mean()
+  grp_CAGR_d_retnStd_std = df_perf['CAGR/return_std'].std()
+  grp_CAGR_d_retnStd_mean_div_std = grp_CAGR_d_retnStd_mean / grp_CAGR_d_retnStd_std
+
+  grp_retnStd_d_UI_mean = df_perf['return_std/UI'].mean()
+  grp_retnStd_d_UI_std = df_perf['return_std/UI'].std()
+  grp_retnStd_d_UI_mean_div_std = grp_retnStd_d_UI_mean / grp_retnStd_d_UI_std
+
+  return df_perf, grp_retnStd_d_UI_mean_div_std, grp_CAGR_d_retnStd_mean_div_std, grp_CAGR_d_UI_mean_div_std
