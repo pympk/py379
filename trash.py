662332b6ae0bcebbed4479329dc28e4fc1affaf6
pympk <yee_ping@yahoo.com>
2022-12-22 15:25:02 -0800

    a

--- trash.py
+++ trash.py
@@ -1,44 +1,143 @@
-def perf_eval(df_close):
-  '''
-  df_close is a dataframe with date index, columns of symbols' closing price, and symbol as column name.
-  Calculate symbols' performance in drawdown, Ulcer-Index, max-drawdown, standard deviation of returns,
-  standard deviation of returns / Ulcer-Index, CAGR, CAGR / standard deviation of returns, CAGR / Ulcer-Index.
 
-  '''
+def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
+    """Returns perf_ranks_dict(dic. of dic. of symbols ranked in descending
+     performance) and ranked_perf_ranks_dict(dic. of symbols ranked in
+     descending frequency in a combined pool of symbols in perf_ranks_dict).
 
-#   from myUtils import symb_perf_stats_vectorized_v2
-  from trash import symb_perf_stats_vectorized_v2  
+    Args:
+        df_close(dataframe): dataframe of symbols' close with
+         DatetimeIndex e.g. (['2016-12-19', ... '2016-12-22']), symbols as
+         column names, and symbols' close as column values.
+        days_lookbacks(list of positive integers): list of number of days to
+        look-back, e.g. [15, 30], for performance calculation.
 
-  symbols, period_yr, drawdown, UI, max_drawdown, returns_std, returns_std_div_UI, CAGR, CAGR_div_returns_std, CAGR_div_UI = \
-      symb_perf_stats_vectorized_v2(df_close)
+    Return:
+        perf_ranks_dict({dic): dic. of dic. of symbols ranked in descending
+         performance.
+         First dic keys are:
+          'period' + str(days_lookbacks[0]), ... ,
+          'period' + str(days_lookbacks[-1])
+         Second dic keys are:
+          'r_CAGR/UI', 'r_CAGR/retnStd' and 'r_retnStd/UI'
+         e.g.:
+          {
+            period-15': {
+                         'r_CAGR/UI':  ['HZNP', ... , 'CB'],
+                         'r_CAGR/retnStd': ['BBW', ... , 'CPRX'],
+                         'r_retnStd/UI':   ['ENR', ... , 'HSY']
+                        },
+            ... ,
+            'period-60': {
+                          'r_CAGR/UI':  ['WNC', ... , 'FSLR'],
+                          'r_CAGR/retnStd': ['VCYT', ... , 'BERY'],
+                          'r_retnStd/UI':   ['MYOV', ... , 'NSC']
+                         }
+          }
+        ranked_perf_ranks_dict(dic): dic. of symbols ranked in descending
+         frequency in a combined pool of symbols in perf_ranks_dict.  Key is
+         'ranked_perf_ranks_period' + str(days_lookbacks), e.g.:
+         {'ranked_perf_ranks_period[-15, -30]': ['HZNP', ... , 'NSC']}
+    """
 
-  caches_perf_stats_vect = []
-  for symbol in symbols:
-      # date_first = drawdown.index[0].strftime('%Y-%m-%d')
-      # date_last = drawdown.index[-1].strftime('%Y-%m-%d')
-      date_first = df_close.index[0].strftime('%Y-%m-%d')
-      date_last = df_close.index[-1].strftime('%Y-%m-%d')
+    import pandas as pd
+    from myUtils import symb_perf_stats_vectorized_v2
 
-      cache = (symbol, date_first, date_last, period_yr, CAGR[symbol],
-              UI[symbol], returns_std_div_UI[symbol], CAGR_div_returns_std[symbol], CAGR_div_UI[symbol])
-      # append performance data (tuple) to caches_perf_stats (list)
-      caches_perf_stats_vect.append(cache)
-  column_names = ['symbol', 'first date', 'last date', 'Year', 'CAGR',
-                  'UI', 'return_std/UI', 'CAGR/return_std', 'CAGR/UI']
-  # write symbols' performance stats to dataframe
-  df_perf = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
+    perf_ranks_dict = {}  # dic of performance ranks
+    syms_perf_rank = []  # list of lists to store top 100 ranked symbols
 
-  _cols = ['CAGR', 'UI', 'retrun_std/UI', 'CAGR/return_std', 'CAGR/UI']
-  grp_CAGR_d_UI_mean = df_perf['CAGR/UI'].mean()
-  grp_CAGR_d_UI_std  = df_perf['CAGR/UI'].std()
-  grp_CAGR_d_UI_mean_div_std = grp_CAGR_d_UI_mean / grp_CAGR_d_UI_std 
+    for days_lookback in days_lookbacks:
+        days_lookback = -1 * days_lookback
+        f_name = "period" + str(days_lookback)
+        _df_c = df_close[days_lookback::]
+        (
+            symbols,
+            period_yr,
+            drawdown,
+            UI,
+            max_drawdown,
+            retnStd,
+            retnStd_d_UI,
+            CAGR,
+            CAGR_d_retnStd,
+            CAGR_d_UI,        
+        ) = symb_perf_stats_vectorized_v2(_df_c)            
 
-  grp_CAGR_d_retnStd_mean = df_perf['CAGR/return_std'].mean()
-  grp_CAGR_d_retnStd_std = df_perf['CAGR/return_std'].std()
-  grp_CAGR_d_retnStd_mean_div_std = grp_CAGR_d_retnStd_mean / grp_CAGR_d_retnStd_std
 
-  grp_retnStd_d_UI_mean = df_perf['return_std/UI'].mean()
-  grp_retnStd_d_UI_std = df_perf['return_std/UI'].std()
-  grp_retnStd_d_UI_mean_div_std = grp_retnStd_d_UI_mean / grp_retnStd_d_UI_std
+        caches_perf_stats_vect = []
+        for symbol in symbols:
+            date_first = drawdown.index[0].strftime("%Y-%m-%d")
+            date_last = drawdown.index[-1].strftime("%Y-%m-%d")
+            cache = (
+                symbol,
+                date_first,
+                date_last,
+                period_yr,
+                CAGR[symbol],
+                UI[symbol],
+                retnStd_d_UI[symbol],
+                CAGR_d_retnStd[symbol],
+                CAGR_d_UI[symbol],
+            )
+            # append performance data (tuple) to caches_perf_stats (list)
+            caches_perf_stats_vect.append(cache)
+        column_names = [
+            "symbol",
+            "first date",
+            "last date",
+            "Year",
+            "CAGR",
+            "UI",
+            "retnStd/UI",
+            "CAGR/retnStd",
+            "CAGR/UI",
+        ]
 
-  return df_perf, grp_retnStd_d_UI_mean_div_std, grp_CAGR_d_retnStd_mean_div_std, grp_CAGR_d_UI_mean_div_std
+        # write symbols' performance stats to dataframe
+        df_ps = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
+        df_ps["r_CAGR/UI"] = df_ps["CAGR/UI"].rank(ascending=False)
+        df_ps["r_CAGR/retnStd"] = df_ps["CAGR/retnStd"].rank(ascending=False)
+        df_ps["r_retnStd/UI"] = df_ps["retnStd/UI"].rank(ascending=False)
+
+        _dict = {}
+        cols_sort = ["r_CAGR/UI", "r_CAGR/retnStd", "r_retnStd/UI"]
+
+        # print(f'{f_name} top 100 symbols')
+        for col in cols_sort:
+            symbols_top_n = (
+
+                # df_ps.sort_values(by=[col]).head(n_symbols).symbol.values
+                df_ps.sort_values(by=[col]).symbol.values
+
+            )
+            syms_perf_rank.append(list(symbols_top_n))
+            # print(f'{col}: {symbols_top_n}')
+            _dict[col] = symbols_top_n
+            perf_ranks_dict[f"{f_name}"] = _dict
+
+    syms_perf_rank  # list of lists of top 100 rank
+    l_syms_perf_rank = [
+        val for sublist in syms_perf_rank for val in sublist
+    ]  # flatten list of lists
+
+    from collections import Counter
+
+    cnt_symbol_freq = Counter(l_syms_perf_rank)  # count symbols and frequency
+    # print(cnt_symbol_freq)
+    l_tuples = (
+        cnt_symbol_freq.most_common()
+    )  # convert to e.g [('AKRO', 6), ('IMVT', 4), ... ('ADEA', 3)]
+    symbols_ranked_perf_ranks = [
+        symbol for symbol, count in l_tuples
+    ]  # select just the symbols without the frequency counts
+
+    ranked_perf_ranks_dict = {}
+    f_name = f"ranked_perf_ranks_period" + str(
+        days_lookbacks
+    )  # key name, ranked_perf_ranks_dict
+    ranked_perf_ranks_dict[
+        f"{f_name}"
+    # values: list of most common symbols in all performance ranks in
+    #  descending order
+    ] = symbols_ranked_perf_ranks
+
+    return perf_ranks_dict, ranked_perf_ranks_dict
--- yf_utils.py
+++ yf_utils.py
@@ -114,19 +114,19 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
           'period' + str(days_lookbacks[0]), ... ,
           'period' + str(days_lookbacks[-1])
          Second dic keys are:
-          'r_CAGR/UI', 'r_CAGR/Std' and 'r_Std/UI'
+          'r_CAGR/UI', 'r_CAGR/retnStd' and 'r_retnStd/UI'
          e.g.:
           {
             period-15': {
                          'r_CAGR/UI':  ['HZNP', ... , 'CB'],
-                         'r_CAGR/Std': ['BBW', ... , 'CPRX'],
-                         'r_Std/UI':   ['ENR', ... , 'HSY']
+                         'r_CAGR/retnStd': ['BBW', ... , 'CPRX'],
+                         'r_retnStd/UI':   ['ENR', ... , 'HSY']
                         },
             ... ,
             'period-60': {
                           'r_CAGR/UI':  ['WNC', ... , 'FSLR'],
-                          'r_CAGR/Std': ['VCYT', ... , 'BERY'],
-                          'r_Std/UI':   ['MYOV', ... , 'NSC']
+                          'r_CAGR/retnStd': ['VCYT', ... , 'BERY'],
+                          'r_retnStd/UI':   ['MYOV', ... , 'NSC']
                          }
           }
         ranked_perf_ranks_dict(dic): dic. of symbols ranked in descending
@@ -135,7 +135,6 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
          {'ranked_perf_ranks_period[-15, -30]': ['HZNP', ... , 'NSC']}
     """
 
-    # from myUtils import pickle_load, pickle_dump, symb_perf_stats_vectorized
     import pandas as pd
     from myUtils import symb_perf_stats_vectorized_v2
 
@@ -143,34 +142,20 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
     syms_perf_rank = []  # list of lists to store top 100 ranked symbols
 
     for days_lookback in days_lookbacks:
-
-
-
         days_lookback = -1 * days_lookback
-
-
-
         f_name = "period" + str(days_lookback)
         _df_c = df_close[days_lookback::]
-
-
-
-        print(f'days_lookback: {days_lookback}')
-        print(f'_df_c: {_df_c}')
-
-
-
         (
             symbols,
             period_yr,
             drawdown,
             UI,
             max_drawdown,
-            returns_std,
-            Std_UI,
+            retnStd,
+            retnStd_d_UI,
             CAGR,
-            CAGR_Std,
-            CAGR_UI,        
+            CAGR_d_retnStd,
+            CAGR_d_UI,        
         ) = symb_perf_stats_vectorized_v2(_df_c)            
 
 
@@ -185,9 +170,9 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
                 period_yr,
                 CAGR[symbol],
                 UI[symbol],
-                Std_UI[symbol],
-                CAGR_Std[symbol],
-                CAGR_UI[symbol],
+                retnStd_d_UI[symbol],
+                CAGR_d_retnStd[symbol],
+                CAGR_d_UI[symbol],
             )
             # append performance data (tuple) to caches_perf_stats (list)
             caches_perf_stats_vect.append(cache)
@@ -198,19 +183,19 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
             "Year",
             "CAGR",
             "UI",
-            "Std/UI",
-            "CAGR/Std",
+            "retnStd/UI",
+            "CAGR/retnStd",
             "CAGR/UI",
         ]
 
         # write symbols' performance stats to dataframe
         df_ps = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
         df_ps["r_CAGR/UI"] = df_ps["CAGR/UI"].rank(ascending=False)
-        df_ps["r_CAGR/Std"] = df_ps["CAGR/Std"].rank(ascending=False)
-        df_ps["r_Std/UI"] = df_ps["Std/UI"].rank(ascending=False)
+        df_ps["r_CAGR/retnStd"] = df_ps["CAGR/retnStd"].rank(ascending=False)
+        df_ps["r_retnStd/UI"] = df_ps["retnStd/UI"].rank(ascending=False)
 
         _dict = {}
-        cols_sort = ["r_CAGR/UI", "r_CAGR/Std", "r_Std/UI"]
+        cols_sort = ["r_CAGR/UI", "r_CAGR/retnStd", "r_retnStd/UI"]
 
         # print(f'{f_name} top 100 symbols')
         for col in cols_sort:
@@ -224,10 +209,6 @@ def _4_perf_ranks(df_close, days_lookbacks, verbose=False):
             # print(f'{col}: {symbols_top_n}')
             _dict[col] = symbols_top_n
             perf_ranks_dict[f"{f_name}"] = _dict
-        # print(' ')
-
-    # pickle_dump(perf_ranks_dict, path_data_dump, f_pickled_perf_ranks_dict)
-    # print(f'perf_ranks_dict:\n{perf_ranks_dict}\n')
 
     syms_perf_rank  # list of lists of top 100 rank
     l_syms_perf_rank = [
