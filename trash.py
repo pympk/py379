8627c6dd1115d3b3ced8f1e2cb0ab9eefb5ec3ad
pympk <yee_ping@yahoo.com>
2023-01-05 20:14:28 -0800

    a

--- trash.py
+++ trash.py
@@ -1,37 +1,14 @@
-def _6_grp_tuples_sort_sum(l_tuples, reverse=True):
-    # https://stackoverflow.com/questions/2249036/grouping-python-tuple-list
-    # https://stackoverflow.com/questions/10695139/sort-a-list-of-tuples-by-2nd-item-integer-value
-    """
-    Given a list of tuples of (key:value) such as:
-    [('grape', 100), ('apple', 15), ('grape', 3), ('apple', 10),
-     ('apple', 4), ('banana', 3)]
-    Returns list of grouped-sorted-tuples based on summed-values such as:
-    [('grape', 103), ('apple', 29), ('banana', 3)]
-
-    Args:
-        l_tuples(list of tuples): list of tuples of key(str):value(int) pairs
-        reverse(bool): sort order of summed-values of the grouped tuples,
-         default is descending order.
-
-    Return:
-        grp_sorted_list(list of tuples): list of grouped-sorted-tuples
-         based on summed-values such as:
-         [('grape', 103), ('apple', 29), ('banana', 3)]
-    """
-
-    import itertools
-    from operator import itemgetter
-
-    grp_list = []
-    sorted_tuples = sorted(l_tuples)
-    it = itertools.groupby(sorted_tuples, itemgetter(0))
-
-    for key, subiter in it:
-        # print(f'key: {key}')
-        key_sum = sum(item[1] for item in subiter)
-        # print(f'key_sum: {key_sum}')
-        grp_list.append((key, key_sum))
-
-    grp_sorted_list = sorted(grp_list, key=itemgetter(1), reverse=reverse)
-
-    return grp_sorted_list
+print('z_grp_top_set_syms:')
+z_grp_top_set_syms = zip(max_lookback_slices, grp_top_set_syms_n_freq)
+for i, (_lookback_slice, _top_set_syms_n_freq) in enumerate(z_grp_top_set_syms):
+  print(f'{i + 1 } of {n_samples} max_lookback_slice')
+  print(f'max_lookback_slice: {_lookback_slice}')
+  print(f'top_set_syms_n_freq: {top_set_syms_n_freq}')
+  print(f'start_eval: {_lookback_slice[1]}')
+  print(f'end_eval:   {_lookback_slice[2]}')
+  l_syms = []  # list to accumlate top set symbbols
+  for sym_n_freq in _top_set_syms_n_freq:
+    l_syms.append(sym_n_freq[0])
+    print(f'symbol: {sym_n_freq[0]:>4},  freq: {sym_n_freq[1]:>2}')
+  print(f'top symbols from max_lookback_slice: {l_syms}')  
+  print('')  
