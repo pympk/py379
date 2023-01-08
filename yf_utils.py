77c2e4306c8b9459afa8e2886dacccf6f9e38513
pympk <yee_ping@yahoo.com>
2023-01-08 12:36:27 -0800

    a

--- yf_utils.py
+++ yf_utils.py
@@ -452,3 +452,92 @@ def _6_grp_tuples_sort_sum(l_tuples, reverse=True):
     grp_sorted_list = sorted(grp_list, key=itemgetter(1), reverse=reverse)
 
     return grp_sorted_list
+
+
+def _7_perf_eval(df_close):
+    """
+    df_close is a dataframe with date index, columns of symbols' closing price, and symbols as column names.
+    The function first calculates symbols' drawdown, Ulcer-Index, max-drawdown, std(returns),
+    std(returns)/Ulcer-Index, CAGR, CAGR/std(returns), CAGR/Ulcer-Index. Then it calculates and returns
+    the group's (i.e. all the symbols) mean, standard-deviation, mean/standard-deviation for
+    'retrun_std/UI', 'CAGR/return_std', 'CAGR/UI'.
+
+    Args:
+      df_close(dataframe): dataframe of symbols' close with
+        DatetimeIndex e.g. (['2016-12-19', ... '2016-12-22']), symbols as
+        column names, and symbols' close as column values.
+
+    Return:
+      df_perf(dataframe): dataframe with columns symbol, first date, last date, Year, CAGR,	UI,
+          return_std/UI, CAGR/return_std, CAGR/UI
+      grp_retnStd_d_UI(list): [(std(returns)/Ulcer-Index).mean, (std(returns)/Ulcer-Index).std,
+        (std(returns)/Ulcer-Index).mean / (std(returns)/Ulcer-Index).std]
+      grp_CAGR_d_retnStd(list): [(CAGR/std(returns)).mean, (CAGR/std(returns)).std,
+        (CAGR/std(returns)).mean / (CAGR/std(returns)).std]
+      grp_CAGR_d_UI(list): [(CAGR/Ulcer_Index).mean, (CAGR/Ulcer_Index).std,
+        (CAGR/Ulcer_Index).mean / (CAGR/Ulcer_Index).std]
+    """
+
+    from myUtils import symb_perf_stats_vectorized_v2
+
+    (
+        symbols,
+        period_yr,
+        drawdown,
+        UI,
+        max_drawdown,
+        returns_std,
+        returns_std_div_UI,
+        CAGR,
+        CAGR_div_returns_std,
+        CAGR_div_UI,
+    ) = symb_perf_stats_vectorized_v2(df_close)
+
+    caches_perf_stats_vect = []
+    for symbol in symbols:
+        date_first = df_close.index[0].strftime("%Y-%m-%d")
+        date_last = df_close.index[-1].strftime("%Y-%m-%d")
+
+        cache = (
+            symbol,
+            date_first,
+            date_last,
+            period_yr,
+            CAGR[symbol],
+            UI[symbol],
+            returns_std_div_UI[symbol],
+            CAGR_div_returns_std[symbol],
+            CAGR_div_UI[symbol],
+        )
+        # append performance data (tuple) to caches_perf_stats (list)
+        caches_perf_stats_vect.append(cache)
+
+    column_names = [
+        "symbol",
+        "first date",
+        "last date",
+        "Year",
+        "CAGR",
+        "UI",
+        "return_std/UI",
+        "CAGR/return_std",
+        "CAGR/UI",
+    ]
+    # write symbols' performance stats to dataframe
+    df_perf = pd.DataFrame(caches_perf_stats_vect, columns=column_names)
+
+    _cols = ["CAGR", "UI", "retrun_std/UI", "CAGR/return_std", "CAGR/UI"]
+
+    _mean = df_perf["CAGR/UI"].mean()
+    _std = df_perf["CAGR/UI"].std()
+    grp_CAGR_d_UI = [_mean, _std, _mean / _std]
+
+    _mean = df_perf["CAGR/return_std"].mean()
+    _std = df_perf["CAGR/return_std"].std()
+    grp_CAGR_d_retnStd = [_mean, _std, _mean / _std]
+
+    _mean = df_perf["return_std/UI"].mean()
+    _std = df_perf["return_std/UI"].std()
+    grp_retnStd_d_UI = [_mean, _std, _mean / _std]
+
+    return df_perf, grp_retnStd_d_UI, grp_CAGR_d_retnStd, grp_CAGR_d_UI
